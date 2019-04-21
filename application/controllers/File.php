284f9991b07f04e3d9c63e2fb937792384737a8c
DenisS <den_sorokin_1@mail.ru>
2019-04-22 01:09:17 +0300

    Added view download

--- application/controllers/File.php
+++ application/controllers/File.php
@@ -19,57 +19,43 @@ class File extends CI_Controller
 
         $this->load->database();
         $this->load->library('upload');
+        $this->load->library('session');
         $this->load->model('Files');
         $this->load->view('header');
         $this->load->view('files/file_sharing_index');
         $this->load->view('footer');
         echo '<pre>';
         $file_key = $this->input->post('file_key');
-        //$this->Files->getByKey($file_key);
 
-        //$link = __DIR__ . '../file_dump/';
-        // /* $tempName =*/ print_r($_FILES);
-        echo '<br>';
-        //print_r($tempName);
-        echo '<br>';
         $name = md5(time() . rand(1, 1000) . $_FILES['userFile']['name']);
         $ext = explode('.', $_FILES['userFile']['name']);
         //unset($ext[0]);
         $ext = $ext[count($ext) - 1];
         $key = $name[0] . $name[1] . $name[2] . $name[3] . $name[4] . $name[5] . $name[6] . $name[7];
-
+        $this->session->keyLink = $key;
         $pathOld = __DIR__ . '/../file_dump/' . $name[0] . '/' . $name[1];//;
-        $path = 
-        $link = $path . $name;
-        $this->Files->saveFile( $link,$key);
-        //echo $_FILES['userFile']['tmp_name'];
-        // $this->Files->do_upload('');
-        //
+        $path = str_replace('\\', '/', $pathOld);
 
-//        echo '<pre>';
-//        print_r($ext);
-//        exit();
+        $link = $path . $name;
+        $this->Files->saveFile($link, $key);
+
         if (!file_exists($path)) {
             mkdir($path, 0777, true);
         }
 
 
-//        foreach($_FILES['pictures'] as $picture) {
-//
-//                $tmp_name = $_FILES['pictures']['tmp_name'][$picture];
-//                $name = $_FILES['pictures']['name'][$picture];
-//                move_uploaded_file($tmp_name, "$path/$name");
-//
-//        }
-
-
         move_uploaded_file($_FILES['userFile']['tmp_name'], $path . '/' . $name . '.' . $ext);
 
-        //echo $file_key;
-        //
-        //print_r($res);
-        // echo $this->db->set('content', 'My Content')->get_compiled_insert();
-        print_r($_POST);
-        print_r($_FILES);
+    }
+
+    public function download()
+    {
+        $key = $this->session->keyLink;
+
+        $this->Files->getByKey($key);
+        //$downloadLink='http://task.by/index.php/File/download/' . $key;
+        $this->load->view('header');
+        $this->load->view('file_download');
+        $this->load->view('footer');
     }
 }
--- application/models/Files.php
+++ application/models/Files.php
@@ -15,38 +15,16 @@ class Files extends CI_Model
         return print_r($query->result());
     }
 
-    public function saveFile($file, $key,$userId =0,$comment='')
+    public function saveFile($file, $key,$userId =0,$comment='No')
     {
         $currDate = time();
 //        $this->db->set('file_link', $file)->get_compiled_insert(self::TABLE,FALSE);
 //        $this->db->set('file_key', $key)->get_compiled_insert(self::TABLE,FALSE);
 //        $this->db->set('user_id', $userId)->get_compiled_insert(self::TABLE,FALSE);
 //        $this->db->set('creation_date', $currDate)->get_compiled_insert(self::TABLE,FALSE);
-        var_dump($file);
+       // var_dump($file);
         $this->db->query("INSERT INTO `files`(`user_id`, `file_key`,`file_link`,`creation_date`,`comment`) values ($userId,'$key','$file',$currDate,'$comment')");
 
     }
-//    public function do_upload($name)
-//    {
-//        $config['upload_path']          = './uploads/';
-//        $config['allowed_types']        = 'gif|jpg|png';
-//        $config['max_size']             = 100;
-//        $config['max_width']            = 1024;
-//        $config['max_height']           = 768;
-//
-//        $this->load->library('upload', $config);
-//
-//        if ( ! $this->upload->do_upload($name))
-//        {
-//            $error = array('error' => $this->upload->display_errors());
-//
-//            $this->load->view('upload_form', $error);
-//        }
-//        else
-//        {
-//            $data = array('upload_data' => $this->upload->data());
-//
-//            $this->load->view('upload_success', $data);
-//        }
-//    }
+
 }
