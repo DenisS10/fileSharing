7242829455f8d3923c147211af7950ba97bc1d75
DenisS <den_sorokin_1@mail.ru>
2019-04-23 22:56:55 +0300

    some changes

--- application/views/files/file_sharing_index.php
+++ application/views/files/file_sharing_index.php
@@ -4,7 +4,8 @@
 <!---->
 <!--        </thead>-->
 <?//
-
+//echo '<pre>';
+//print_r($imgArr);
 //$name = '/1/e1e7077e05f1066d28d4c9a9f5d86bbed.png';
 //?>
 <!--        <tbody>-->
@@ -20,12 +21,29 @@
         <input type="file" name="userFile">
         <button class="" >Send</button>
     </form>
-</div>
+
 <div>
     <form class="" method="post" action="">
         <label>Enter link</label>
         <input  name="file_key">
+
 <!--        <img src="">-->
         <button class="" type="submit" >Get file!</button>
     </form>
+</div>
+</div>
+
+<div>
+    <ul style="list-style-type: none;">
+    <? foreach ($imgArr as $img) {
+//        echo '<pre>';
+//       print_r($img);
+
+
+        ?>
+<li>
+    <a href="download/<?=$img->file_key?>"><?if($img->extension == 'jpg' || $img->extension == 'png' || $img->extension == 'gif' || $img->extension == 'JPG'|| $img->extension == 'PNG'|| $img->extension == 'GIF'){?><img src="<?=$img->file_link?>"><?}else echo 'Download file';?></a><!-- style="width:82px; height:86px"-->
+</li>
+    <?}?>
+</ul>
 </div>
